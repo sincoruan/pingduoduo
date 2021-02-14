e25ba4bbb667e5a1bceb1a49ad225d6f49a79b43
ruanxingke <rxingke@gmail.com>
2021-02-14 15:07:13 -0600

    4-13 依赖注入(1)

--- src/app/home/components/home-grand/home-grand.component.ts
+++ src/app/home/components/home-grand/home-grand.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, Injector, OnInit } from '@angular/core';
+//@Injectable()
+class Product{
+  //一个声明干了两件事：1>添加了成员变量 2>构造函数增加了这个参数
+  constructor(private name:string ){
 
+  }
+}
+class PurchaseOrder{
+  private product:Product;
+  private count:number;
+  constructor(product:Product){
+    this.product = product;
+  }
+}
 @Component({
   selector: 'app-home-grand',
   templateUrl: './home-grand.component.html',
@@ -14,7 +27,27 @@ export class HomeGrandComponent implements OnInit {
 
   ngOnInit() {
     this.date = this.minusDays(new Date(),1);
-    this.price = 1234.5678
+    this.price = 1234.5678;
+    const injector = Injector.create({
+      providers:[
+        {
+          provide: Product,
+          useFactory:()=>{
+            return new Product('大米手机')
+          },
+          useClass: Product,
+          deps:[]
+        },
+        {
+          provide: PurchaseOrder,
+          useClass: PurchaseOrder,
+          deps:[Product]
+        }
+      ]
+    });
+    console.log(injector.get(Product));
+    console.log(injector.get(PurchaseOrder));
+    
   }
 
   minusDays(date:Date,days:number){
