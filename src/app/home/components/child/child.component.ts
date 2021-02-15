c2eb24fb3d328e0569069fd1b42d48d422bee087
ruanxingke <rxingke@gmail.com>
2021-02-15 17:43:40 -0600

    4-17 脏值检测(3)

--- src/app/home/components/child/child.component.ts
+++ src/app/home/components/child/child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, ElementRef, NgZone, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,19 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   _title;
+  _time;
+
+  @ViewChild('timeRef',{static:true}) timeRef:ElementRef;
+
+  public get time():number{
+    console.log(this._time);
+    return this._time;
+  }
   public get title():string{
     console.log("脏值检测");
     return this._title;
   }
-  constructor() {
-    this._title="hello"
+  constructor(private ngZone:NgZone,private rd2:Renderer2) {
+    this._title="hello";
    }
 
   ngOnInit() {
   }
   ngAfterViewChecked(): void {
     //不能在脏值检测中对绑定变量重新赋值
-    this._title = "23";
+    // this._title = "23";
+    this.ngZone.runOutsideAngular(()=>{
+      setInterval(()=>{
+        this._title="nihao";
+        this.rd2.setProperty(this.timeRef.nativeElement,
+          'innerText',
+          Date.now()
+        );
+        
+      },100);
+    });
   }
+  handleClickViewCheck(){
 
+  }
 }
