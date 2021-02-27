b87e50c9e3917d4b195de39c874d4e9446f4407b
ruanxingke <rxingke@gmail.com>
2021-02-27 15:53:52 -0600

    5-6 rxjs 初步了解

--- src/app/home/components/parent/parent.component.ts
+++ src/app/home/components/parent/parent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  @ViewChild('inputRef',{static:true}) inputRef:ElementRef;
   constructor() { }
 
   ngOnInit() {
+    fromEvent(this.inputRef.nativeElement,'input')
+    .subscribe(
+      (ev:any) => 
+      console.log(ev.target.value) 
+      ) ;
+    console.log("hello");
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ChildComponent, HomeAuxComponent, HomeContainerComponent, HomeDetailComponent,HomeGrandComponent, ParentComponent } from './components';
+import {  HomeAuxComponent, HomeContainerComponent, HomeDetailComponent,HomeGrandComponent, ParentComponent } from './components';
 import { HomeService, token } from './services';
 
 
 @NgModule({
-  declarations: [HomeContainerComponent,HomeDetailComponent,HomeGrandComponent,HomeAuxComponent,ParentComponent,ChildComponent],
+  declarations: [HomeContainerComponent,HomeDetailComponent,HomeGrandComponent,HomeAuxComponent,ParentComponent],
   providers:[
     HomeService,
     {provide:token,useValue:'http://local.dev'}
