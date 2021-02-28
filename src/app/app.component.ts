c884eb3c56d73d5724bfa65aa9865b70407d1766
ruanxingke <rxingke@gmail.com>
2021-02-28 13:54:31 -0600

    6-2 创建底部 TabBar(2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { TabItem } from './shared/domain';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +15,31 @@ export class AppComponent {
     //Called after every check of the component's view. Applies to components only.
     //Add 'implements AfterViewChecked' to the class.
     // console.log(this.imgSlider);
+  }
+  constructor(private router:Router){
+
+  }
+  ngOnInit(): void {
+    this.selectedIndex$ = this.router.events.pipe(
+      filter(ev=>ev instanceof NavigationEnd),
+      map((ev:NavigationEnd)=>{
+        const arr = ev.url.split('/');
+        return arr.length>1?arr[1]:'home';
+      }),
+      map(path=>this.getSelectedIndex(path))
+    );
+    
+  }
+  getSelectedIndex(tab:string):number{
+    return 
+    tab === 'recommend'  ? 1 
+    : tab === 'category' ? 2 
+    : tab === 'chat'     ? 3 
+    : tab === 'my'       ? 4 
+    : 0;
+  }
+  selectedIndex$: Observable<number>;
+  handleTabSelected(tab:TabItem){
+    this.router.navigate([tab.link]);
   }
 }
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -11,15 +11,27 @@ export class FooterComponent implements OnInit {
   tabItems: TabItem[]=[
     {
       title:'聊天',
-      icon:'/assets/tabs/chat.png',
-      link:'chat',
-      selectedIcon:'/assets/tabs/chat_selected.png'
+      icon:'/assets/tabs/recommend.png',
+      link:'recommend',
+      selectedIcon:'/assets/tabs/recommend_selected.png'
+    },
+    {
+      title:'聊天',
+      icon:'/assets/tabs/category.png',
+      link:'category',
+      selectedIcon:'/assets/tabs/category_selected.png'
     },
     {
       title:'聊天',
       icon:'/assets/tabs/chat.png',
       link:'chat',
       selectedIcon:'/assets/tabs/chat_selected.png'
+    },
+    {
+      title:'聊天',
+      icon:'/assets/tabs/my.png',
+      link:'my',
+      selectedIcon:'/assets/tabs/my_selected.png'
     }
   ];
   @Input() selectedIndex=0;
