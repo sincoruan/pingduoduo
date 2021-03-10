8690340e78281e792573bfdf5ca20b380f5f2372
ruanxingke <rxingke@gmail.com>
2021-03-10 17:43:50 -0600

    7-4 实战拼单列表

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ProductContainerComponent } from './components';
+import { GroupItemComponent, GroupShortListComponent, ProductContainerComponent } from './components';
 
 
 @NgModule({
   declarations: [
-    ProductContainerComponent
+    ProductContainerComponent,
+    GroupItemComponent,
+    GroupShortListComponent
   ],
   imports: [
     SharedModule,
